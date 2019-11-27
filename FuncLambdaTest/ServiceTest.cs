20e844891ab98b12482f5ce7c4456c1790a803bf
Raul .A Alzate <raul.alzate@techandsolve.com>
2019-11-27 08:29:28 -0500

    add project test

---
+++ FuncLambdaTest/ServiceTest.cs
@@ -0,0 +1,17 @@
+using Xunit;
+using Moq;
+
+namespace microserviceTest
+{
+    public class ServiceTest
+    {
+
+
+        [Fact]
+        public void Test()
+        {
+             Assert.Equal(10, 10);
+
+        }
+    }
+}
